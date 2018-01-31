48a1d63984cd3f6c914ddf26fd429456e3ee7cb1
dvn14 <dvn14@ic.ac.uk>
2018-01-31 23:42:23 +0000

    added an if statement for the get_entropy function to take into account 0*log(0)

--- get_entropy.m
+++ get_entropy.m
@@ -3,7 +3,11 @@ function entropy = get_entropy(binary_targets)
 [p,~] = size(find(binary_targets == 1));
 [n,~] = size(find(binary_targets == 0));
 
-entropy = -(p/(p+n))*log2(p/(p+n))-(n/(p+n))*log2(n/(p+n));
+if( (p==0) || (n==0))
+    entropy = 0;
+else
+    entropy = -(p/(p+n))*log2(p/(p+n))-(n/(p+n))*log2(n/(p+n));
+end
 
 end
 
