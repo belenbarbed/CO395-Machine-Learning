b4a8feecdd584d36aae19b1ad5fda2a71067bf85
Piotr Pomienski <p.pomien@gmail.com>
2018-02-08 12:52:55 +0000

    working n-fold tree learning

--- run_trees.m
+++ run_trees.m
@@ -1,21 +1,47 @@
-function run_trees()
+function trees=run_trees()
 
 A = load('cleandata_students.mat');
 all_exs = A.x;
 all_tgts = A.y;
 
-part_exs = partition(all_exs);
-part_tgts = partition(all_tgts);
+[e1, e2, e3, e4, e5, e6, e7, e8, e9, e10] = partition(all_exs);
+[t1, t2, t3, t4, t5, t6, t7, t8, t9, t10] = partition(all_tgts);
 
-for i=1:1:10
-    tmp_e = part_exs;
-    tmp_e(i) = [];
-    examples(i) = squeeze(tmp_e);
-    tmp_t = part_tgts;
-    tmp_t(i) = [];
-    targets(i) = squeeze(tmp_t);
-    
-    tree(i) = decision_tree(examples(i), targets(i));
+examples = {};
+targets = {};
+
+examples = [examples, [e2; e3; e4; e5; e6; e7; e8; e9; e10]];
+targets = [targets, [t2; t3; t4; t5; t6; t7; t8; t9; t10]];
+
+examples = [examples, [e1; e3; e4; e5; e6; e7; e8; e9; e10]];
+targets = [targets, [t1; t3; t4; t5; t6; t7; t8; t9; t10]];
+
+examples = [examples, [e1; e2; e4; e5; e6; e7; e8; e9; e10]];
+targets = [targets, [t1; t2; t4; t5; t6; t7; t8; t9; t10]];
+
+examples = [examples, [e1; e2; e3; e5; e6; e7; e8; e9; e10]];
+targets = [targets, [t1; t2; t3; t5; t6; t7; t8; t9; t10]];
+
+examples = [examples, [e1; e2; e3; e4; e6; e7; e8; e9; e10]];
+targets = [targets, [t1; t2; t3; t4; t6; t7; t8; t9; t10]];
+
+examples = [examples, [e1; e2; e3; e4; e5; e7; e8; e9; e10]];
+targets = [targets, [t1; t2; t3; t4; t5; t7; t8; t9; t10]];
+
+examples = [examples, [e1; e2; e3; e4; e5; e6; e8; e9; e10]];
+targets = [targets, [t1; t2; t3; t4; t5; t6; t8; t9; t10]];
+
+examples = [examples, [e1; e2; e3; e4; e5; e6; e7; e9; e10]];
+targets = [targets, [t1; t2; t3; t4; t5; t6; t7; t9; t10]];
+
+examples = [examples, [e1; e2; e3; e4; e5; e6; e7; e8; e10]];
+targets = [targets, [t1; t2; t3; t4; t5; t6; t7; t8; t10]];
+
+examples = [examples, [e1; e2; e3; e4; e5; e6; e7; e8; e9]];
+targets = [targets, [t1; t2; t3; t4; t5; t6; t7; t8; t9]];
+
+for i=1:1:10   
+    trees{i} = decision_tree(examples{i}, targets{i});
 end
 
 end
