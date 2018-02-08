b4968dfa08091fe99d6a89c42ba26a51db53d905
dvn14 <dvn14@ic.ac.uk>
2018-02-08 13:20:42 +0000

    the function evaluation_matrix outputs all evaluation aspects on one table

--- evaluation_matrix.m
+++ evaluation_matrix.m
@@ -1,15 +1,16 @@
-function [eval,CR] = evaluation_matrix(C)
+function T = evaluation_matrix(C)
 
-eval = zeros(3,6);
-%                      |_________class_________|
-%______________________|_1_|_2_|_3_|_4_|_5_|_6_|
-%(row1)Recall rate     |   |   |   |   |   |   |
-%(row2)Precision rate  |   |   |   |   |   |   |
-%(row3)F1 measure      |   |   |   |   |   |   |
-%-----------------------------------------------
+eval = zeros(5,7);
+%                      |_________class_________|___|
+%______________________|_1_|_2_|_3_|_4_|_5_|_6_|ACR|
+%(row1)Recall rate     |   |   |   |   |   |   |NaN|
+%(row2)Precision rate  |   |   |   |   |   |   |NaN|
+%(row3)F1 measure      |   |   |   |   |   |   |NaN|
+%(row4)CR              |   |   |   |   |   |   |NaN|   
+%(row5)Average CR      |NaN|NaN|NaN|NaN|NaN|NaN|   |
+%---------------------------------------------------
 Total_TP = 0;
 Total = sum(sum(C));
-CR_array = zeros(1,6);
 
 for i=1:1:6
    TP = C(i,i);
@@ -27,9 +28,15 @@ for i=1:1:6
    %F1 measure
    eval(3,i) = 2*(Pr*Rr)/(Pr+Rr);
    %CR
-   CR_array(i) = 100*(TP+TN)/Total;
+   eval(4,i) = 100*(TP+TN)/Total;
+   eval(5,i) = NaN;
 end
 
-CR = mean(CR_array);
+eval(:,7) = NaN;
+eval(5,7) = mean(eval(4,1:6));
+
+colNames = {'Class_1','Class_2','Class_3','Class_4','Class_5','Class_6','Average_CR'};
+rowNames = {'Recall Rate','Precision Rate','F1 Measure','CR','Average CR'};
+T = array2table(eval,'RowNames',rowNames,'VariableNames',colNames);
 
 end
