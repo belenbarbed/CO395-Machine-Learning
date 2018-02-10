fd8a5833464b69889fcccd8613fa941e2f6d7b90
Piotr Pomienski <p.pomien@gmail.com>
2018-02-10 01:52:42 +0000

    add conf matrix, statistics and various fixes

--- run_trees.m
+++ run_trees.m
@@ -1,8 +1,10 @@
-function trees=run_trees()
+function [trees, conf, eval]=run_trees()
 
 A = load('cleandata_students.mat');
 all_exs = A.x;
 all_tgts = A.y;
+
+total_size = numel(all_tgts);
 
 [e1, e2, e3, e4, e5, e6, e7, e8, e9, e10] = partition(all_exs);
 [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10] = partition(all_tgts);
@@ -41,17 +43,18 @@ examples = [examples, [e1; e2; e3; e4; e5; e6; e7; e8; e9]];
 targets = [targets, [t1; t2; t3; t4; t5; t6; t7; t8; t9]];
 
 test_examples = {e1, e2, e3, e4, e5, e6, e7, e8, e9, e10};
-actual_tgt = {t1, t2, t3, t4, t5, t6, t7, t8, t9, t10};
+%actual_tgt = {t1, t2, t3, t4, t5, t6, t7, t8, t9, t10};
+predict_tgt = [];
 
 for i=1:1:10  
     trees{i} = decision_tree(examples{i}, targets{i});
     [test_rows, ~] = size(test_examples{i});
-    
     %preallocate the prediction vector
-    predict_tgt = zeros(test_rows,1);
+    
     
     % for each row in examples, classify the example using the tree
     for r=1:test_rows
+        
         % test for each of the 6 emotions
         bin_test = [];
         for e=1:1:6
@@ -64,26 +67,29 @@ for i=1:1:10
                 bin_test = [bin_test, e];
             end
         end
-        
+
         %check how many solutions we have
         sols_found = numel(bin_test);
         
         %ideal scenario - we are sure an example is emotion e
         if (sols_found == 1)
-            predict_tgt(r) = bin_test(1);
+            predict_tgt = [predict_tgt; bin_test(1)];
             
         %non-ideal scenario - we need to pick one of the possible options
         elseif (sols_found > 1)
             %TODO: make this do something smarter than just picking the
             %first one...
-            predict_tgt(r) = bin_test(1);
+            predict_tgt = [predict_tgt; bin_test(1)];
         else
-            fprintf("Mo solutions found for fold %i example %i.\n", i, r)
+            %FIXME: this does not belong here
+            predict_tgt = [predict_tgt; 1];
+            fprintf("No solutions found for fold %i example %i.\n", i, r)
         end
     end
-    
-    %DO SOME STATS HERE
-    
-end
 
+end
+    disp(size(predict_tgt))
+    conf = confusion_matrix(all_tgts, predict_tgt)
+    eval = evaluation_matrix(conf)
+    %DO SOME STATS HERE
 end
