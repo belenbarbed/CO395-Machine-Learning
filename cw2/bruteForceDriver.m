249aff2b2548cc67b109ec3c25aa2450b5696e8f
Devin Nanayakkara <dvn14@eews506a-027.ee.ic.ac.uk>
2018-03-01 18:04:43 +0000

    got 43% testCR

--- cw2/bruteForceDriver.m
+++ cw2/bruteForceDriver.m
@@ -2,12 +2,12 @@ function bruteForceDriver
 
     hiddenLayers = [500 500 1000 7];
     epoch = 50;
-    initialLR = 0.01;
-    lrEpochThres = 5;
-    momentumEpochLowerThres = 5;
-    momentumEpochUpperThres = 50;
+    initialLR = 0.02;
+    lrEpochThres = 10;
+    momentumEpochLowerThres = 10;
+    momentumEpochUpperThres = 25;
     hiddenActivationFunctions = {'ReLu','ReLu','sigm','softmax'};
-    weightInit = 3;
+    weightInit = 8;
 
     bruteForce(hiddenLayers, epoch, initialLR, lrEpochThres, momentumEpochLowerThres, momentumEpochUpperThres, hiddenActivationFunctions, weightInit);
 end
